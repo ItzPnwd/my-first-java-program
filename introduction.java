4491cfb96a5b341fafbe4374585aef5274c43013
Sean Donlin <itzpnwd115@gmail.com>
2017-08-25 09:52:30 -0400

    Initial sharing of project

---
+++ introduction.java
@@ -0,0 +1,29 @@
+/**
+ * This program tells a few things about me.
+ * 
+ * @author Sean Donlin
+ * @version August 22, 2017
+ */
+
+public class  introduction
+{
+    public static void main(String[] args)
+    {
+        //here are some facts about me
+        System.out.println("My name is Sean Donlin");
+        System.out.println("I was born in Scranton, Pennsylvania");
+        System.out.println("I go to J.L. Mann Highschool");
+        System.out.println("I am 15 years old");
+        System.out.println("I was born in 2001");
+        System.out.println("I like to drive and run");
+        
+        //now store some numbers
+        int year = 2017;
+        int age = 15;
+        
+        //What happens here?
+        System.out.println("year - age");
+        System.out.println(year - age);
+        System.out.println("I am " + age + " years old.");
+    }
+}
